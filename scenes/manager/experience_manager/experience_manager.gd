81b2d991ea3f2971f167b1f29c620c4258c2c253
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-11-06 14:24:43 -0600

    implement ability upgrade resources

--- scenes/manager/experience_manager/experience_manager.gd
+++ scenes/manager/experience_manager/experience_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal experience_updated(current_experience: float, target_experience: float)
+signal level_up(new_level:int)
 const TARGET_EXP_GROWTH = 5
 var current_experience = 0
 var current_level = 1
@@ -17,6 +18,7 @@ func increment_experience(number: float):
 		current_level += 1
 		target_experience += TARGET_EXP_GROWTH
 		current_experience = 0
-	experience_updated.emit(current_experience, target_experience)
+		experience_updated.emit(current_experience, target_experience)
+		level_up.emit(current_level)
 func on_experience_vial_collected(number: float):
 	increment_experience(number)
