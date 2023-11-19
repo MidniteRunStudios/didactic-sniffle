be75508183ebe810c5fd3963aff803895704335c
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-11-19 00:06:19 -0600

    player health

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
+@onready var damage_interval_timer = $DamageIntervalTimer
+
 const ACCELERATION_SMOOTHING = 25
 const MAX_SPEED = 125
-# Called when the node enters the scene tree for the first time.
+
+var number_colliding_bodies = 0
+
 func _ready():
-	pass # Replace with function body.
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	# normalize vector to fix diagonal issues causing vectors to exceed 1.
@@ -16,8 +21,26 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	#processes velocity
 	move_and_slide()
+
 # AN: Find the difference of X and Y coordinates from user unput
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement,y_movement)
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+
+func on_body_entered(other_body:Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+	
+func on_body_exited(other_body:Node2D):
+	number_colliding_bodies -= 1
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
