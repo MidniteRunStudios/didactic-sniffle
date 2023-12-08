85288b61848dd22d601cd66e9b80f06fe73b0cb1
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-12-08 16:45:26 -0600

    fix end screen freeze when restarting

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -10,7 +10,7 @@ func set_defeat():
 	$%DescriptionLabel.text = "You lost!"
 	
 func on_restart_button_pressed():
-	get_tree().paused = true
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 	
 func on_quit_button_pressed():
