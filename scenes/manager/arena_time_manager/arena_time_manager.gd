151c3e6b4108abbe97410f915139013025987339
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-12-07 20:44:50 -0600

    implemented victory screen

--- scenes/manager/arena_time_manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager/arena_time_manager.gd
@@ -1,6 +1,13 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
 @onready var timer: Timer = $Timer
+
+func _ready():
+	$Timer.timeout.connect(on_timer_timeout)
 # Called when the node enters the scene tree for the first time.
 func get_time_elasped():
 	return timer.wait_time - timer.time_left
+func on_timer_timeout():
+	var victory_screen_instance = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
