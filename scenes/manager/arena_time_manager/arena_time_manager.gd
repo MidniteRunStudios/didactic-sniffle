d531420a38b9a07e7b0962a53b49c00b745915bd
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-12-08 13:21:00 -0600

    refactored victoryscreen to endscreen

--- scenes/manager/arena_time_manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager/arena_time_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var victory_screen_scene: PackedScene
+@export var end_screen_scene: PackedScene
 @onready var timer: Timer = $Timer
 
 func _ready():
@@ -9,5 +9,5 @@ func _ready():
 func get_time_elasped():
 	return timer.wait_time - timer.time_left
 func on_timer_timeout():
-	var victory_screen_instance = victory_screen_scene.instantiate()
-	add_child(victory_screen_instance)
+	var end_screen_instance = end_screen_scene.instantiate()
+	add_child(end_screen_instance)
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -5,8 +5,12 @@ func _ready():
 	$%RestartButton.pressed.connect(on_restart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
 	
+func set_defeat():
+	$%TitleLabel.text = "Defeat"
+	$%DescriptionLabel.text = "You lost!"
+	
 func on_restart_button_pressed():
-	get_tree().paused = false
+	get_tree().paused = true
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 	
 func on_quit_button_pressed():
