ea849a76b7cbbaea729be82bed814e726d4cf339
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-12-30 16:53:57 -0600

    implement floating damage

--- scenes/component/health/hurt_box_component.gd
+++ scenes/component/health/hurt_box_component.gd
@@ -2,6 +2,8 @@ extends Area2D
 class_name HurtboxComponent
 
 @export var health_component: Node
+
+var floating_textbox_scene = preload("res://scenes/ui/floating_text.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	area_entered.connect(on_area_entered) # Replace with function body.
@@ -15,3 +17,8 @@ func on_area_entered(other_area: Area2D):
 		return
 	var hitbox_component = other_area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text_instance = floating_textbox_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text_instance)
+	floating_text_instance.global_position = global_position + (Vector2.UP * 16)
+	floating_text_instance.start(str(hitbox_component.damage))
