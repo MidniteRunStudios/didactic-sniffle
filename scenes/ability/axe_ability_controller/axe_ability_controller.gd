0c4c431fec48c1d19cbfe73c5487a658e1ac815b
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-12-31 16:09:35 -0600

    implement axe and sword damage upgrades + weight loot table

--- scenes/ability/axe_ability_controller/axe_ability_controller.gd
+++ scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -1,10 +1,14 @@
 extends Node
 
 @export var axe_ability_scene: PackedScene
-var damage = 10
+var base_damage = 10
+var additional_damage_percent = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout) # Replace with function body.
+	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_added)
+ # Replace with function body.
 
 func on_timer_timeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
@@ -18,5 +22,9 @@ func on_timer_timeout():
 	var axe_ability_instance = axe_ability_scene.instantiate() as Node2D
 	foreground.add_child(axe_ability_instance)
 	axe_ability_instance.global_position = player.global_position
-	axe_ability_instance.hitbox_component.damage = damage
+	axe_ability_instance.hitbox_component.damage = base_damage * additional_damage_percent
+
+func on_ability_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id == "axe_damage":
+		additional_damage_percent = 1 + (current_upgrades["axe_damage"]["quantity"] * 0.1)
 
