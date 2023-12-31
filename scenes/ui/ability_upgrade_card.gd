89520b76fe963722e13494e3c73d247891005fef
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2023-12-31 16:10:14 -0600

    implement basic theme for upgrade cards + animations

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -5,8 +5,11 @@ signal selected
 @onready var name_label: Label = $%NameLabel
 @onready var description_label: Label = $%DesciptionLabel
 
+var disabled = false
+
 func _ready():
 	gui_input.connect(_on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
 
 func set_ability_upgrade(ability: AbilityUpgrade):
 	name_label.text = ability.name
@@ -14,5 +17,33 @@ func set_ability_upgrade(ability: AbilityUpgrade):
 
 
 func _on_gui_input(event: InputEvent):
+	if disabled:
+		return
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+		discard_other_cards()
+
+func play_in(delay:float = 0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	$AnimationPlayer.play("in")
+
+func play_discard():
+	$AnimationPlayer.play("discard")
+
+func select_card():
+	disabled = true
+	$AnimationPlayer.play("selected")
+	await $AnimationPlayer.animation_finished
+	selected.emit()
+	
+func discard_other_cards():
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+
+func on_mouse_entered():
+	if disabled:
+		return
+	$HoverAnimationPlayer.play("hover")
