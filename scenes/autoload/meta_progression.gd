16af74db43e166a678168fcf7154f7e000a51114
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-01-06 17:26:38 -0600

    imeplement meta upgrade card

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,16 +1,34 @@
 extends Node
 
+const SAVE_FILE_PATH: String = "user://game.save"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
-	"meta upgrades": {}
+	"meta_upgrades": {}
 }
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_exp_vial_collected)
+	load_save_file()
 	
+func load_save_file():
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file = FileAccess.open(SAVE_FILE_PATH,FileAccess.READ)
+	save_data = file.get_var()
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH,FileAccess.WRITE)
+	file.store_var(save_data)
+
 func add_meta_upgrade(upgrade: MetaUpgrade):
-	save_data
+	if !save_data["meta_upgrades"].has(upgrade.id):
+		save_data["meta_upgrades"][upgrade.id] = {
+			"quantity": 0
+		}
+	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
+	save()
 
 func on_exp_vial_collected(experience: float):
 	save_data["meta_upgrade_currency"] += experience
-	
+	save()
---
+++ scenes/ui/meta_upgrade_card.gd
@@ -0,0 +1,44 @@
+extends PanelContainer
+
+
+@onready var name_label: Label = $%NameLabel
+@onready var description_label: Label = $%DesciptionLabel
+@onready var purchase_button = $%PurchaseButton
+@onready var progress_bar = $%ProgressBar
+@onready var progress_label = %ProgressLabel
+
+
+
+var upgrade: MetaUpgrade
+
+func _ready():
+	purchase_button.pressed.connect(on_purchase_button_pressed)
+
+func set_meta_upgrade(upgrade: MetaUpgrade):
+	self.upgrade = upgrade
+	name_label.text = upgrade.title
+	description_label.text = upgrade.description
+	update_progress()
+
+
+func select_card():
+	$AnimationPlayer.play("selected")
+	await $AnimationPlayer.animation_finished
+	
+func on_purchase_button_pressed():
+	if upgrade == null:
+		return
+	MetaProgression.add_meta_upgrade(upgrade)
+	MetaProgression.save_data["meta_upgrade_currency"] -= upgrade.experience_cost
+	MetaProgression.save()
+	get_tree().call_group("meta_upgrade_card", "update_progress")
+	select_card()
+
+
+func update_progress():
+	var currency = MetaProgression.save_data["meta_upgrade_currency"]
+	var percent = currency / upgrade.experience_cost
+	percent = min(percent, 1)
+	progress_bar.value = percent
+	purchase_button.disabled = percent < 1
+	progress_label.text = str(currency) + "/" + str(upgrade.experience_cost)
