3f15bbe475e0562f8eb7bfff12befa2c926118e3
Nathaniel Townsend <cybernetikshadow@hotmail.com>
2024-01-11 21:06:08 -0600

    add health regen upgrade

--- scenes/component/health/health_component.gd
+++ scenes/component/health/health_component.gd
@@ -6,6 +6,7 @@ var current_health: float
 
 signal died
 signal health_changed
+signal health_decreased
 
 func _ready():
 	current_health = max_health
@@ -13,7 +14,12 @@ func _ready():
 func damage(damage_amount: float):
 	current_health = max(current_health - damage_amount, 0)
 	health_changed.emit()
+	if damage_amount > 0:
+		health_decreased.emit()
 	Callable(check_death).call_deferred()
+
+func heal(heal_amount: int):
+	damage(-heal_amount)
 
 func get_health_percent():
 	if max_health <= 0:
